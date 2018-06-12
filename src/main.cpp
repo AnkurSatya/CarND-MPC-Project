031f48700b740ef3daafb4fefa6d13a7caabe758
Ankur Satya <satya.ankur@gmail.com>
2018-06-13 00:53:28 +0530

    Final Submission

--- src/main.cpp
+++ src/main.cpp
@@ -65,6 +65,22 @@ Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
   return result;
 }
 
+vector<double> transform_coordinates(double ref_x, double ref_y, double car_x, double car_y, double psi)
+{
+  //ref_x and ref_y are the coordinates of the point which needs to be transformed.
+  //global_x and global_y are the coordinates of the car.
+
+  double x = ref_x - car_x;
+  double y = ref_y  - car_y;
+
+  //Map coordinates were left handed and car coordinates are right handed.
+
+  double temp_x = x * cos(psi) + y * sin(psi);
+  double temp_y = -x * sin(psi) + y * cos(psi);
+
+  return {temp_x, temp_y};
+}
+
 int main() {
   uWS::Hub h;
 
@@ -98,13 +114,57 @@ int main() {
           * Both are in between [-1, 1].
           *
           */
-          double steer_value;
-          double throttle_value;
+
+          Eigen::VectorXd xvals(ptsx.size());
+          Eigen::VectorXd yvals(ptsy.size());
+
+          //Transforming the waypoints to the car frame from the global frame.
+
+          for(int i = 0; i<ptsx.size(); i++)
+          {
+            auto temp = transform_coordinates(ptsx[i], ptsy[i], px, py, psi);
+            xvals[i] = temp[0];
+            yvals[i] = temp[1];
+          }
+
+          // std::cout<<"Path Values - "<<'\n'<<xvals[0]<<'\n'<<yvals[0];
+
+          auto coeffs = polyfit(xvals, yvals, 3);//change epsi when changing order of the polynomial.
+
+          //The x, y and psi for the vehicle will be zero here as the coordinates are in the car coordinate system.
+          //0 in the equations represent these variables.
+
+          double cte = - polyeval(coeffs, 0);//as everything is now in car coordinates, px will become zero. We take cte = ideal position - actual position
+          //and that is why we have taken (f0 - y0) in model constraints in mpc.cpp.
+          
+          double epsi = 0 - atan(coeffs[1]);
+
+          Eigen::VectorXd state(6);
+          state<<0, 0, 0, v, cte, epsi;
+
+          //Including latency.
+
+          double latency = 0.1;
+          double Lf = 2.67;
+          double steer_value = j[1]["steering_angle"];
+          double throttle_value = j[1]["throttle"];
+          
+          state[0]+= v * cos(0) * latency;
+          state[1]+= v * sin(0) * latency;
+          state[2]+= -steer_value * v * latency/Lf;
+          state[3]+= throttle_value * latency;
+          state[4]+= v * sin(state[5]) * latency;
+          state[5]+= -steer_value * v * latency/Lf;
+
+          auto new_state = mpc.Solve(state, coeffs);
+
+          steer_value = -new_state[0];
+          throttle_value = new_state[1];
 
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
-          msgJson["steering_angle"] = steer_value;
+          msgJson["steering_angle"] = steer_value/deg2rad(25);
           msgJson["throttle"] = throttle_value;
 
           //Display the MPC predicted trajectory 
@@ -113,6 +173,19 @@ int main() {
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
+
+          for(int i = 2; i<new_state.size(); i++)
+          {
+            if(i%2 == 0)
+            {
+              mpc_x_vals.push_back(new_state[i]);
+            }
+            else
+            {
+              mpc_y_vals.push_back(new_state[i]);
+            }
+          }
+          // std::cout<<"MPC Values - "<<'\n'<<mpc_x_vals[0]<<'\n'<<mpc_y_vals[0];
 
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
@@ -123,10 +196,14 @@ int main() {
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
+          for(int i = 0; i<50; i+=3)
+          {
+            next_x_vals.push_back(i);
+            next_y_vals.push_back(polyeval(coeffs, i));
+          }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
-
 
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
